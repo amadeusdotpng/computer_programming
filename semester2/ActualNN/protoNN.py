89ddff3f67362faaa9e006678e922d7eac673b6d
Amadeus8182 <amadeus8182@gmail.com>
2023-04-04 12:30:25 -0500

    added rust to data and fixed model to actually train on 3/4 of the data

--- semester2/ActualNN/protoNN.py
+++ semester2/ActualNN/protoNN.py
@@ -5,7 +5,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neural_network import MLPClassifier
 from sklearn.utils import shuffle
 
-with open('data/keywords.txt', 'r') as f:
+with open('keywords.txt', 'r') as f:
     keywords = np.array([line.strip() for line in f.readlines()])
 
 vectorizer = TfidfVectorizer(lowercase=False)
@@ -16,7 +16,7 @@ y = []
 
 def add_files(dataList, targetList, dir, target):
     for i, file in enumerate(os.listdir(dir)):
-        if i >= 400:
+        if i >= 500:
             break
         
         file = open(os.path.join(dir, file))
@@ -29,12 +29,14 @@ def add_files(dataList, targetList, dir, target):
 add_files(X, y, './data/java'  , 'java')
 add_files(X, y, './data/cpp'   , 'cpp')
 add_files(X, y, './data/python', 'python')
+add_files(X, y, './data/rust'  , 'rust')
 
 X, y = shuffle(X, y)
 print(np.array(X).shape)
-clf = MLPClassifier(hidden_layer_sizes=[100,100,100], max_iter=10000000, verbose=True)
-clf.fit(X[:300], y[:300])
-print(clf.score(X[300:], y[300:]))
+clf = MLPClassifier(hidden_layer_sizes=[1000,1000,1000], max_iter=10000000, verbose=True)
+# clf = MLPClassifier(max_iter=10000000, verbose=True)
+clf.fit(X[:400*4], y[:400*4])
+print(clf.score(X[400*4:], y[400*4:]))
 
 with open('model.pkl', 'wb') as f:
     pickle.dump(clf, f)
