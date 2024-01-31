ce7b3e62a865aebefacdcd8a5bebef732044fa34
Amadeus8182 <amadeus8182@gmail.com>
2024-01-30 21:38:35 -0600

    BFSDFS: part one -> move solution check inside edge loop, measure time, add error messages

--- CP3/bfsdfs/transformation.py
+++ CP3/bfsdfs/transformation.py
@@ -1,6 +1,7 @@
 from collections import deque
 from typing import Set
 import sys
+import time
 
 def get_next_words(word: str, dictionary: Set):
     word = [s for s in word]
@@ -42,11 +43,6 @@ def bfs(root_word: str, goal_word: str, dictionary: Set):
         pop_count += 1
         depth, last_word = len(chain)-1, chain[-1]
 
-        if last_word == goal_word:
-            solutions.add(chain)
-            if not max_depth:
-                max_depth = depth
-            continue
 
         for next_word in get_next_words(last_word, dictionary):
             if max_depth and depth+1 > max_depth:
@@ -58,6 +54,13 @@ def bfs(root_word: str, goal_word: str, dictionary: Set):
             new_chain = (*chain, next_word)
 
             visited[next_word] = depth+1
+
+            if next_word == goal_word:
+                solutions.add(new_chain)
+                if not max_depth:
+                    max_depth = depth+1
+                continue
+
             Q.append(new_chain)
 
     return solutions, max_depth, pop_count
@@ -73,7 +76,17 @@ if __name__ == '__main__':
         start = sys.argv[1].upper()
         end = sys.argv[2].upper()
 
+        if start not in words:
+            print('the starting word must be included in the dictionary')
+            exit(1)
+        if end not in words:
+            print('the ending word must be included in the dictionary')
+            exit(1)
+
+        t0 = time.time()
         paths, length, pop_count = bfs(start, end, words)
+        t = time.time()-t0
+        print(f'time taken to calculate: {t}')
         print(f'number of pop operations: {pop_count}')
         print(f'number of transformations: {length}')
         print('\n'.join(' -> '.join(t) for t in paths))
